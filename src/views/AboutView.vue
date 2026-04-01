4e46c31796650a9a944ee6e1f345b221514d0f74
Doug Gainey <douggainey@gmail.com>
2026-04-01 15:26:31 -0400

    Tweaks.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,9 @@
 <template>
   <div>
     <h2>About Me</h2>
-    <p>
-      Christian. Husband. Father. Software Engineer.
-    </p>
-    <p>
-      Outside of work, I enjoy traveling with my family, cheering on my kids at their latest sporting event and getting offshore whenever I can to fish.
-    </p>
-    <p>
-      Professionally, I'm an Enterprise Software Architect with 20+ years of experience and a passion for building practical systems that make a positive human impact.
-    </p>
+    <p>Christian. Husband. Father. Software Engineer.</p>
+    <p>Outside of work, I enjoy traveling with my family, cheering on my kids at their latest sporting event and getting offshore whenever I can to fish.</p>
+    <p>Professionally, I'm a Principal Software Engineer with 20+ years of experience and a passion for building practical systems that make a positive human impact.</p>
     <p class="flex flex--gapped">
       <i class="material-icons md-person"></i>
       Doug Gainey in Florence, SC
